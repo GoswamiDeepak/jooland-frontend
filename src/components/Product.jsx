ff788ef61c9a0758cd81b822698ad3079e409165
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-09-09 23:22:50 +0530

    add div for sort

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ChevronDown } from 'lucide-react';
 import { Card } from './Card';
+import DynamiceText from './DynamiceText';
 
 const filters = [
     {
@@ -41,13 +42,42 @@ const filters = [
 ];
 
 const MobileFilter = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    // Toggle visibility of the dropdown
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
     return (
         <div className="mt-6 flex items-center  pt-2 md:mt-0 md:space-x-4  md:pt-0">
-            <button
-                type="button"
-                className="hidden items-center rounded-md px-3 py-2 text-sm font-semibold text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black lg:inline-flex">
-                Sort <ChevronDown className="ml-2 h-4 w-4" />
-            </button>
+            <div className="relative">
+                <button
+                    type="button"
+                    onClick={toggleDropdown}
+                    className="hidden items-center rounded-md px-3 py-2 text-sm font-semibold text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black lg:inline-flex">
+                    Sort <ChevronDown className="ml-2 h-4 w-4" />
+                </button>
+
+                <div
+                    className={`w-[170px] h-auto text-right absolute right-0 pt-6 pr-7 pb-4 shadow-lg rounded-sm bg-white  transition-all duration-300 ease-in-out transform ${
+                        isOpen
+                            ? 'translate-y-0 opacity-100 visible'
+                            : '-translate-y-4 opacity-0 invisible'
+                    }`}>
+                    <buttton className="block w-full  cursor-pointer hover:font-medium">
+                        Featured
+                    </buttton>
+                    <buttton className="block w-full cursor-pointer hover:font-medium">
+                        Newest
+                    </buttton>
+                    <buttton className="block w-full cursor-pointer hover:font-medium">
+                        Price: High-Low
+                    </buttton>
+                    <buttton className="block w-full cursor-pointer hover:font-medium">
+                        rice: Low-High
+                    </buttton>
+                </div>
+            </div>
             <button
                 type="button"
                 className="inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black lg:hidden">
@@ -68,13 +98,15 @@ const MobileFilter = () => {
 };
 
 export function Product() {
+    console.log('render...');
+
     return (
         <section className="w-full">
             <div className="mx-auto max-w-7xl px-2 py-10 lg:px-10">
                 <div className="md:flex md:flex-row md:items-start md:justify-between">
-                    <div>
-                        <h1 className="text-xl font-bold">Products</h1>
-                    </div>
+                    <DynamiceText elementType="h1" style="text-xl font-bold">
+                        Product
+                    </DynamiceText>
                     <MobileFilter />
                 </div>
                 <hr className="my-8" />
