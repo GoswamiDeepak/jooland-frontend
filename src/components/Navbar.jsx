a2716e63b33e5882a8c507560d6713ffbcd156c2
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-09-09 20:17:51 +0530

    modified in moblie navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -129,6 +129,38 @@ export function Navbar() {
         }
     };
 
+    const MobileNavbar = () => {
+        return (
+            isMenuOpen && (
+                <div className="absolute inset-x-0 top-0 z-50 origin-top-right transform p-2 transition lg:hidden">
+                    <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                        <div className="px-5 pb-6 pt-0">
+                            <div className="flex items-center justify-between">
+                                <div className="inline-flex items-center space-x-2">
+                                    <Logo />
+                                </div>
+                                <div className="-mr-2">
+                                    <ButtonType
+                                        className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                                        onClickHandler={toggleMenu}
+                                        type="button">
+                                        <X
+                                            className="h-6 w-6"
+                                            aria-hidden="true"
+                                        />
+                                    </ButtonType>
+                                </div>
+                            </div>
+                            <div className="mt-6">
+                                <MobileNav />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        );
+    };
+
     return (
         <div className="relative w-full bg-white">
             <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
@@ -141,34 +173,7 @@ export function Navbar() {
                         className="h-6 w-6 cursor-pointer"
                     />
                 </div>
-                {/* is menu open*/}
-                {isMenuOpen && (
-                    <div className="absolute inset-x-0 top-0 z-50 origin-top-right transform p-2 transition lg:hidden">
-                        <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-                            <div className="px-5 pb-6 pt-5">
-                                <div className="flex items-center justify-between">
-                                    <div className="inline-flex items-center space-x-2">
-                                        <Logo />
-                                    </div>
-                                    <div className="-mr-2">
-                                        <ButtonType
-                                            className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                                            onClickHandler={toggleMenu}
-                                            type="button">
-                                            <X
-                                                className="h-6 w-6"
-                                                aria-hidden="true"
-                                            />
-                                        </ButtonType>
-                                    </div>
-                                </div>
-                                <div className="mt-6">
-                                    <MobileNav />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
+                <MobileNavbar />
             </div>
         </div>
     );
